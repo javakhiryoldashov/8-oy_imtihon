a9b2137a3063eb05e7c52478959253bf942e6655
javokhiryoldashov <yuldoshovjavohir3@gmail.com>
2024-08-16 06:38:20 +0500

    fenish

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+vite.config.json
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    outputDirectory: 'dist',
+  },
+});
